28567fa42dfc3f936c794864307805fdfcbee092
Olga Alekhina <alekhina.olga@gmail.com>
2023-01-13 17:37:16 +0300

    update date_notes page

--- Noted/notes/views.py
+++ Noted/notes/views.py
@@ -4,33 +4,33 @@ from .models import Note, Tag
 from .forms import NoteForm
 
 
-# выводит главную страницу с заметками на текущую неделю
+# выводит главную страницу с задачами на текущую неделю
 class NotesList(ListView):
     model = Note
     template_name = 'main.html'
     context_object_name = 'notes'
 
 
-# выводит страницу с одной заметкой
+# выводит страницу с одной задачей
 class NoteDetail(DetailView):
     model = Note
     template_name = 'note.html'
     context_object_name = 'note'
 
 
-# выводит страницу с формой для добавления новой заметки
+# выводит страницу с формой для добавления новой задачи
 class NoteAddView(CreateView):
     template_name = 'note_add_form.html'
     form_class = NoteForm
 
-    # это чтобы автором заметки автоматически считался текущий юзер
+    # это чтобы автором задачи автоматически считался текущий юзер
     def get_initial(self):
         initial = super().get_initial()
         initial['note_author'] = self.request.user
         return initial
 
 
-# выводит форму для редактирования заметки
+# выводит форму для редактирования задачи
 class NoteUpdateView(UpdateView):
     template_name = 'note_update_form.html'
     form_class = NoteForm
@@ -40,21 +40,24 @@ class NoteUpdateView(UpdateView):
         return Note.objects.get(pk=id)
 
 
-# выводит страницу подтверждение удаления заметки
+# выводит страницу подтверждение удаления задачи
 class NoteDeleteView(DeleteView):
     template_name = 'note_delete_form.html'
     queryset = Note.objects.all()
     success_url = '/main/'
 
 
-# выводит страницу с заметками на конкретную дату
+# выводит страницу с задачами на конкретную дату
 class DateList(ListView):
     model = Note
     template_name = 'date_notes.html'
-    context_object_name = 'notes'
 
-    # получить дату из параметра url
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs.filter(note_time=self.kwargs['date']).order_by('-note_priority')
+    # выводит 3 отдельных qs для задач с разным приоритетом + текущую дату
+    def get_context_data(self, **kwargs):
+        context = super(DateList, self).get_context_data(**kwargs)
+        context['notes_low'] = Note.objects.filter(note_time=self.kwargs['date']).filter(note_priority=0)
+        context['notes_normal'] = Note.objects.filter(note_time=self.kwargs['date']).filter(note_priority=1)
+        context['notes_high'] = Note.objects.filter(note_time=self.kwargs['date']).filter(note_priority=2)
+        context['date'] = self.kwargs['date']
+        return context
 
