649f7e08d292d6442704b02bb43f55e6160bf7ab
OlgaAlekhina <olga-alekhina@rambler.ru>
2023-02-20 13:14:59 +0300

    add header and main markup

--- Noted/Noted/settings.py
+++ Noted/Noted/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'notes',
     'accounts',
     'six',
-    'django_bootstrap5',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
