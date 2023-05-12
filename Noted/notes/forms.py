bf4f1289e84f30c7db631d14c207337a977fff90
Olga Alekhina <alekhina.olga@gmail.com>
2023-05-12 17:42:47 +0300

    add and change a lot of styles

--- Noted/notes/forms.py
+++ Noted/notes/forms.py
@@ -34,7 +34,7 @@ class UpdateUserForm(forms.ModelForm):
 # форма для редактирования аватара
 class UpdateProfileForm(forms.ModelForm):
     name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control avatar-file'}))
 
     class Meta:
         model = UserProfile
