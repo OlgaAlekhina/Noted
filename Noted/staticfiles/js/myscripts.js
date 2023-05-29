bdab705860d3539d556baea0ac3ea74f8afa8816
Olga Alekhina <alekhina.olga@gmail.com>
2023-05-29 19:57:51 +0300

    add translations to customized file inputs

--- Noted/staticfiles/js/myscripts.js
+++ Noted/staticfiles/js/myscripts.js
@@ -39,22 +39,26 @@ function subformbtn() {
     setTimeout(function(){document.getElementById("form_submit").style.display= "block";},3000);
 }
 
+// меняет приоритет задачи с низкого на высокий при клике на белый флажок
 function checkBox() {
     var check_on = '<a onclick="uncheckBox()" style="cursor: pointer;"><img src="/static/images/icons/flag_icon.svg" width="25px" height="25px"/></a><span style="padding-left: 4px;">Приоритет</span><input type="hidden" id="priority" name="priority" value="priority">';
     document.getElementById('prior_choice').innerHTML = check_on;
 }
 
+// меняет приоритет задачи с высокого на низкий при клике на красный флажок
 function uncheckBox() {
     var check_on = '<a onclick="checkBox()" style="cursor: pointer;"><img src="/static/images/icons/flag_white.svg" width="25px" height="25px"/></a><span style="padding-left: 4px;">Приоритет</span>';
     document.getElementById('prior_choice').innerHTML = check_on;
 }
 
+// добавляет в урл для восстановления задачи текущую дату, чтобы поставить ее в качестве даты выполнения
 function restoreTask(task_id) {
     date = GetDatestring();
     var url = "/main/task_restore/" + date + '/' + task_id;
     window.location.replace(url);
 }
 
+// показывает или скрывает пароль при нажатии на иконку глаза
 function togglePassword() {
     const password = document.getElementById("password");
     const type = password.getAttribute('type') === "password" ? "text" : "password";
@@ -64,19 +68,29 @@ function togglePassword() {
     img.setAttribute("src", src);
 }
 
+// кастомизация поля выбора файла
 function fileinputChange() {
     var file = document.getElementById('note-file');
-    var placeholder = document.getElementById('text_input_id')
+    var placeholder = document.getElementById('text_input_id');
     if (file.value == "")
     {
-        alert('no file');
-        placeholder.setAttribute("placeholder", "Файл ни фига не выбран");
+        placeholder.setAttribute("placeholder", "Файл не выбран");
     }
     else
     {
         var theSplit = file.value.split('\\');
-        alert(theSplit);
         placeholder.setAttribute("placeholder", theSplit[theSplit.length-1]);
+    }
+}
+
+// переводит текст поля выбора файлов на английский
+function transAddfile() {
+    var file_span = document.getElementById('text_input_span_id');
+    var file_text = document.getElementById('text_input_id');
+    var lang = document.getElementById('lang_select').value;
+    if (lang == 'en') {
+        file_span.innerHTML = 'Add file';
+        file_text.setAttribute("placeholder", "No file chosen");
     }
 }
 
